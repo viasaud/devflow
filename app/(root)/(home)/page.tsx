4edc623ad30a79feea78acc1d227528c2ca7d33e
Saud <viasaud@gmail.com>
2024-01-28 21:57:40 +0300

    Clerk is added for auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
