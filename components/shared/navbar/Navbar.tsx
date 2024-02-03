51eb634912f1bf81a07e060f26421814efc8eebe
Saud <viasaud@gmail.com>
2024-02-03 12:24:03 +0300

    shadcn/ui added

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between bg-light-900_dark-200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between bg-light-900_dark-200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -31,6 +31,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
+        Mob
       </div>
     </nav>
   );
