975cc579afb6e8ce80201de819b9a4ee642b817c
Saud <viasaud@gmail.com>
2024-03-30 16:57:30 +0300

    Revert "Remove unnecessary public route from authMiddleware configuration"
    This reverts commit ed772b5fb80b3c225c3d020fd2c5fcc79447624d.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
+  publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
