ed772b5fb80b3c225c3d020fd2c5fcc79447624d
Saud <viasaud@gmail.com>
2024-03-30 16:48:38 +0300

    Remove unnecessary public route from authMiddleware configuration

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
+  publicRoutes: ["/", "question/:id", "/tags", "/tags/:id", "/profile/:id", "/community", "/jobs"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
