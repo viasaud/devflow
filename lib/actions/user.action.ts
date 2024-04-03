94eb99ab2cbb91a1bb96ffff0932cfb7fcac12f9
Saud <viasaud@gmail.com>
2024-04-03 21:27:30 +0300

    Refactor getSavedQuestions function parameters

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -106,7 +106,7 @@ export const toggleSaveQuestion = async (params: toggleSaveQuestionParams) => {
 export const getSavedQuestions = async (params: getSavedQuestionsParams) => {
   try {
     connectToDatabase();
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const { clerkId, searchQuery } = params;
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
       : {};
