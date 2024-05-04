38a7bbee26ad84a2dfe5970b1f8805684ebf1ef0
Saud <viasaud@gmail.com>
2024-05-04 11:10:56 +0300

    chore: Update select and menubar styles for better user experience

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,7 +10,7 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState(localStorage.theme || "");
 
   const handleThemeChange = () => {
     if (
